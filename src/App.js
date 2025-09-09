87aa5398a2edb6f56f320ca67f2dfdd33249d10f
Munal Jain <75110825+munal0803@users.noreply.github.com>
2025-09-09 14:12:47 +0530

    Setup GitHub Pages deployment from master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
@@ -14,26 +14,19 @@ import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-	}, []);
-
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/MunalJainPortfolio" element={<Homepage />} />
-				<Route path="/" element={<Homepage />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/articles" element={<Articles />} />
-				<Route path="/article/:slug" element={<ReadArticle />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Notfound />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <BrowserRouter basename="/MunalJainPortfolio">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/article/:slug" element={<ReadArticle />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
