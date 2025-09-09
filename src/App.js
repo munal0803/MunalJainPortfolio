928b3802c3cea0a9eead533795557fe2138c930b
Munal Jain <75110825+munal0803@users.noreply.github.com>
2025-09-09 17:03:25 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<Homepage />} />
+				<Route path="/MunalJainPortfolio" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/articles" element={<Articles />} />
